cbe67cbd15b059fdaa7bf3a829d4e2ae0caf6f73
arijoki <ari.joki@gmail.com>
2021-06-25 12:13:09 +0800

    changes to china, not 100% done

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ module.exports.index = (req, res) => {
     let inputAddressLine4 = req.body.address4;
     let inputAddressLine5 = req.body.address5;
     let country = req.body.country;
+    let inputWechat = req.body.wechat;
     // lets validate inputs first
     const schema = Joi.object({
       name: Joi.string().min(3).max(35).required(),
@@ -26,16 +27,31 @@ module.exports.index = (req, res) => {
       phone: Joi.string().min(8).max(20).required(),
       title: Joi.string().min(4).max(25).required(),
       title2: Joi.string().min(4).max(25),
-      skype: Joi.string().min(4).max(20).required(),
+      skype: Joi.string().min(4).max(30).required(),
       address: Joi.string().min(4).max(45).required(),
       address2: Joi.string().min(4).max(45).required(),
-      address3: Joi.string().min(4).max(45),
-      address4: Joi.string().min(4).max(45),
-      address5: Joi.string().min(4).max(45)
+      address3: Joi.string().min(4).max(45).when("country", {
+        is: "China",
+        then: Joi.required()
+      }),
+      address4: Joi.string().min(4).max(45).when("country", {
+        is: "China",
+        then: Joi.required()
+      }),
+      address5: Joi.string().min(4).max(45).when("country", {
+        is: "China",
+        then: Joi.required()
+      }),
+      country: Joi.string(),
+      wechat: Joi.string().min(4).max(45).when("country", {
+        is: "China",
+        then: Joi.required()
+      })
+
 
     });
 
-    let payload = schema.validate({ name: inputName, email: inputEmail, phone: inputPhone, skype: inputSkype, title: inputTitle, name2: inputNameLine2, title2: inputTitleLine2, address: inputAddress, address2: inputAddressLine2, address3: inputAddressLine3, address4: inputAddressLine4, address5: inputAddressLine5});
+    let payload = schema.validate({ name: inputName, email: inputEmail, phone: inputPhone, skype: inputSkype, title: inputTitle, name2: inputNameLine2, title2: inputTitleLine2, address: inputAddress, address2: inputAddressLine2, address3: inputAddressLine3, address4: inputAddressLine4, address5: inputAddressLine5, wechat: inputWechat, country:country});
     if(payload.error  !== undefined) {
       // res.send(tmp1);
       res.status(400).send({'error' : payload.error});
@@ -44,30 +60,43 @@ module.exports.index = (req, res) => {
     var fs = require('fs');
 
 
-    const tag = fs.readFileSync('public/images/sg-namecard.svg','utf8');
-
+    if(country === "China"){
+       var  nameCard = fs.readFileSync('public/images/china-namecard.svg','utf8');
+    }else{
+       var  nameCard = fs.readFileSync('public/images/sg-namecard.svg','utf8');
+    }
     const cheerio = require('cheerio');
-    const $ = cheerio.load(tag);
+    const $ = cheerio.load(nameCard);
     const email = $('.user-email');
     const address1 = $('.address-1');
     const address2 = $('.address-2');
+    const address3 = $('.address-3');
+    const address4 = $('.address-4');
+    const address5 = $('.address-5');
     const skype = $('.user-skype');
     const name = $('.user-name1');
     const name2 = $('.user-name2');
     const title = $('.user-title1');
     const title2 = $('.user-title2');
     const phone = $('.user-phone');
-
+    const wechat = $('.user-wechat');
     email.text(inputEmail);
     skype.text(inputSkype);
     name.text(inputName);
     name2.text(inputNameLine2);
     title.text(inputTitle);
     title2.text(inputTitleLine2);
-    console.log(inputPhone);
     phone.text(inputPhone);
     address1.text(inputAddress);
     address2.text(inputAddressLine2);
+    if(country === "China"){
+      console.log(inputAddressLine3);
+
+      address4.text(inputAddressLine4);
+      address5.text(inputAddressLine5);
+      wechat.text(inputWechat);
+      address3.text(inputAddressLine3);
+    }
     res.header("Content-Type","image/svg+xml");
     res.status(200);
     res.send($.html());
